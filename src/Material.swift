8a2a3a9782885cc9fe75c770b7819ab305da7e0c
George King <george.w.king@gmail.com>
2015-04-06 19:58:45 +0200

    Material is a class.

--- src/Material.swift
+++ src/Material.swift
@@ -4,9 +4,14 @@
 import Foundation
 
 
-struct Material {
+class Material {
   let isLight: Bool
   let col: V3D
+  
+  init(isLight: Bool, col: V3D) {
+    self.isLight = isLight
+    self.col = col
+  }
 }
 
 
