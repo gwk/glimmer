f286cb16317cd06e3e4966c10c8d4b08405e8dd0
George King <george.w.king@gmail.com>
2015-04-06 23:31:26 -0400

    Pixel now stores color directly instead of accumulator.

--- src/Pixel.swift
+++ src/Pixel.swift
@@ -5,28 +5,28 @@ import Foundation
 
 
 struct Pixel {
-  let accum: V3D
+  let col: V3D
   let count: Double
   
   init() {
-    accum = V3D()
+    col = V3D()
     count = 0
   }
   
   init(sample: V3D) {
-    accum = sample
+    col = sample
     count = 1
   }
   
   init(prev: Pixel, col: V3D) {
-    accum = prev.accum + col
-    count = prev.count + 1
+    let n = prev.count
+    let n1 = n + 1
+    self.col = (prev.col * n + col) / n1
+    self.count = n1
   }
   
-  var col: V3D { return count < 1 ? V3D() : (accum / count).clampToUnit }
-  
   var colU8: (U8, U8, U8) { // TODO: use V3U8.
-    let c = col
+    let c = col.clampToUnit
     return (U8(c.x * 255), U8(c.y * 255), U8(c.z * 255))
   }
 }
